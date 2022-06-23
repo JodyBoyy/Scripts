ecb7b50bea952111f348b9a13d7c770b1e538881
JodyBoyy <102689170+JodyBoyy@users.noreply.github.com>
2022-06-23 18:32:59 +0430

    Create ufw_configer.sh
    Using a file, this script configures ufw firewall. It's written in bash and only uses IP and port for it's input. This version can take only on IP and many ports as input.

---
+++ ufw_configer.sh
@@ -0,0 +1,34 @@
+#defining algorithm ip=(2n-1) port=(2n)
+#considering number of lines
+
+#putting the file in descriptor
+exec 13<$@
+#using for-loop and maximum(by seq and wc) 
+LENTH=$(wc -l < $@)
+let "x = $LENTH - 1"
+#reading line by line
+IFS='\n'
+
+readarray -u13 rows
+IFS=' '
+#file pattern requires this sequnce to consider 2n
+#untill it reaches the max value
+WEIGHT=$(seq -s " " 0 2 $x)
+#looping through and matching ip to ports
+for INF in $WEIGHT
+do
+export INF
+        for ANY in ${rows[$(($INF+1))]}                                #trying to get the ports only
+        do
+                export ANY
+                
+                IFS=' '                                                #trying to seperate ports
+                read -a ANY_PORTS <<< ${ANY}                           #trying to output ports each by each
+                
+		            IPADD="$(tr -d "\n" <<< ${rows["${INF}"]})"            #removing newline from IPs
+                sudo ufw allow from ${IPADD} to any port $ANY_PORTS    #where magic happens
+
+        done
+done
+
+#have not checked if ufw is installed
