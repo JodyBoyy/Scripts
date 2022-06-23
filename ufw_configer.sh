c01da25ed612c81b21e38cecf6681343b08e4d6a
JodyBoyy <102689170+JodyBoyy@users.noreply.github.com>
2022-06-23 18:45:54 +0430

    Update ufw_configer.sh
    #### please give our script access permission for execution####
    In command-line:   
    # ./ufw_configer.sh input.txt
    ufw_configer.sh => being the script
    input.txt             => being the input(where we put our IP and ports)

--- ufw_configer.sh
+++ ufw_configer.sh
@@ -3,14 +3,16 @@
 
 #putting the file in descriptor
 exec 13<$@
+
 #using for-loop and maximum(by seq and wc) 
 LENTH=$(wc -l < $@)
 let "x = $LENTH - 1"
+
 #reading line by line
 IFS='\n'
-
 readarray -u13 rows
 IFS=' '
+
 #file pattern requires this sequnce to consider 2n
 #untill it reaches the max value
 WEIGHT=$(seq -s " " 0 2 $x)
@@ -24,11 +26,10 @@ export INF
                 
                 IFS=' '                                                #trying to seperate ports
                 read -a ANY_PORTS <<< ${ANY}                           #trying to output ports each by each
-                
-		            IPADD="$(tr -d "\n" <<< ${rows["${INF}"]})"            #removing newline from IPs
+                IPADD="$(tr -d "\n" <<< ${rows["${INF}"]})"            #removing newline from IPs
                 sudo ufw allow from ${IPADD} to any port $ANY_PORTS    #where magic happens
 
         done
 done
 
-#have not checked if ufw is installed
+#have not checked if ufw is installed or enabled
